8f70d554d14823224cf5997db9a8bde86289cc7f
stephen <stephenrs7@gmail.com>
2017-12-21 19:53:55 -0800

    asdap

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'oAMnbU6arCQ6dGHEl4GjpcCVo',
+  consumer_secret: 'VsHWYOE0OKX3g8H6Vq3NQRx4MupQAFKLXxrXABpy9AYZ36HVGc',
+  access_token_key: '943245352487890944-dUOwAzuNAZGVFgSJ6HeR4nLBqA2EFH7',
+  access_token_secret: 'cjZJbVjKEs3UgzvLscXD4yp8nPiZzSRptB1QxsSGG5YrO',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,9 @@
+// get user input from terminal
+process.argv[2];
+// set variable
+var request = require("request");
+var Twitter = require('twitter');
+// require keys.js
+
+// if user inputs my-tweets, return tweets
+
