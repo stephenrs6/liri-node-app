4dfe8cdc3390d970625372e1934b6149fd0eb9d8
stephen <stephenrs7@gmail.com>
2017-12-23 17:57:49 -0800

    almost last commit

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var request = require("request");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-var omdb = require('omdb');
+var omdb = require('omdbapi');
 // get user input from terminal
 var userInput = process.argv[2];
 // require keys.js
@@ -32,24 +32,92 @@ if (userInput === "spotify-this-song") {
         id: "ba5465286b5e4f928426b8f5de440696",
         secret: "7ff62881976b4fc7b6f4f103d5679ff5",
     });
-
-    spotify.search({ type: 'track', query: process.argv[3] }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
+    var nodeArgs = process.argv;
+    var songName = "";
+    for (var i = 3; i < nodeArgs.length; i++) {
+        if (i > 3 && i < nodeArgs.length) {
+            songName = songName + "+" + nodeArgs[i];
         }
-        var artists = data.tracks.items[0].artists;
-        for (i=0; i <artists.length; i++){
-            console.log(artists[i].name);
+        else {
+            songName += nodeArgs[i];
         }
-        
-        var name = data.tracks.items[0].name;
-        console.log(name);
+    }
 
-        var album = data.tracks.items[0].album.name;
-        console.log(album);
-      
-        var preview = data.tracks.items[0].preview_url;
-        console.log(preview);
+        spotify.search({ type: 'track', query: songName}, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            var artists = data.tracks.items[0].artists;
+            for (i = 0; i < artists.length; i++) {
+                console.log("Artist: " + artists[i].name);
+            }
 
-    })
-};
+            var name = data.tracks.items[0].name;
+            console.log("Track: " + name);
+
+            var album = data.tracks.items[0].album.name;
+            console.log("Album: " + album);
+
+            var preview = data.tracks.items[0].preview_url;
+            console.log("Preview: " + preview);
+
+        })
+    };
+
+    if (userInput === "movie-this") {
+        var nodeArgs = process.argv;
+        var movieName = "";
+        for (var i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+                movieName = movieName + "+" + nodeArgs[i];
+            }
+            else {
+                movieName += nodeArgs[i];
+            }
+        }
+
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+        request(queryUrl, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                console.log("Country: " + JSON.parse(body).Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+            }
+        });
+    }
+
+if (userInput === "do-what-it-says") {
+    fs.readFile('random.txt', 'utf8', function(err, data) {
+      var arr = data.split('\n');
+      var firstLine = arr[0];
+
+      var spotify = new Spotify({
+        id: "ba5465286b5e4f928426b8f5de440696",
+        secret: "7ff62881976b4fc7b6f4f103d5679ff5",
+    });
+        spotify.search({ type: 'track', query: firstLine.split(',')[1]}, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            var artists = data.tracks.items[0].artists;
+            for (i = 0; i < artists.length; i++) {
+                console.log("Artist: " + artists[i].name);
+            }
+
+            var name = data.tracks.items[0].name;
+            console.log("Track: " + name);
+
+            var album = data.tracks.items[0].album.name;
+            console.log("Album: " + album);
+
+            var preview = data.tracks.items[0].preview_url;
+            console.log("Preview: " + preview);
+
+        })
+    })}
